ecc06f3cf9ad142062501eeb738b2eccc0e1ab80
MTLab1122 <muhammadtalhapro1@gmail.com>
2023-08-10 07:02:57 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+//Beginner 1: Create an array and print its content.
+const names = ['Khan', 'Jan', 'Shakir', 'Abdullah'];
+console.log(`The content of this array is: ${names}`);
+//Beginner 2: Add an element to an array.
+const names1 = ['Khan', 'Jan', 'Shakir', 'Abdullah'];
+names1.push('Talha');
+console.log(`The new array is : ${names1}`);
+//Beginner 3: Remove an element from an array.
+const names2 = ['Khan', 'Jan', 'Shakir', 'Abdullah'];
+names2.pop();
+console.log(`The array after remove an element is : ${names2}`);
+//Beginner 4: Sort an array.
+const names3 = ['Khan', 'Jan', 'Shakir', 'Abdullah'];
+names3.sort();
+console.log(names3);
---
+++ index.ts
@@ -0,0 +1,18 @@
+//Beginner 1: Create an array and print its content.
+const names:string[]=['Khan' , 'Jan' , 'Shakir', 'Abdullah'];
+console.log(`The content of this array is: ${names}`);
+
+//Beginner 2: Add an element to an array.
+const names1:string[]=['Khan' , 'Jan' , 'Shakir', 'Abdullah'];
+names1.push('Talha');
+console.log(`The new array is : ${names1}`);
+
+//Beginner 3: Remove an element from an array.
+const names2:string[]=['Khan' , 'Jan' , 'Shakir', 'Abdullah'];
+names2.pop();
+console.log(`The array after remove an element is : ${names2}`);
+
+//Beginner 4: Sort an array.
+const names3:string[]=['Khan' , 'Jan' , 'Shakir', 'Abdullah'];
+names3.sort();
+console.log(names3);
